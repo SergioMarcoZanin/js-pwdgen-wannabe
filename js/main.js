082ebc7738112ae06ca91048204f5bde17cfbee7
SergioMarcoZanin <zaninsergiomarco@gmail.com>
2022-10-20 10:09:08 +0200

    inserimento  prompt

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Chiedi all'utente il suo nome,
+const firssergiotname = prompt ("Inserisci il tuo nome");
+// poi chiedi il suo cognome,
+const lastname = prompt ("Inserisci il tuo cognome");
+// poi chiedi il suo colore preferito
+const color = prompt ("inserisci il tuo colore preferito");
+// Infine scrivi sulla pagin nomecognomecolorepreferito76
+document.getElementById('password').innerHTML = `${firstName}${lastName}${color} 76`;
+console.log(firstName, lastName, color);
